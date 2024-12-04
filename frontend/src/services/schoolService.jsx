37f5b4eb85de027dede2328493b6fdec19c481df
Deepak Choudhary <163335534+Deepak-C-Choudhary@users.noreply.github.com>
2024-12-04 11:12:25 +0530

    Update schoolService.jsx

--- frontend/src/services/schoolService.jsx
+++ frontend/src/services/schoolService.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://complete-school-management.onrender.com";
 
 export const addSchool = async (schoolData) => {
   try {
